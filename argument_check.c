d75305f238eb6e257aef5c08ee57ac0833fe47d4
Santiago Castillo <sancastillog@gmail.com>
2020-05-14 19:58:03 +0000

    negative fix

--- argument_check.c
+++ argument_check.c
@@ -10,7 +10,7 @@ bool is_intiger(char *token)
 
 	for (i = 0; token && token[i]; i++)
 	{
-		if (token[0] == '-')
+		if (i == 0 && token[i] == '-')
 			continue;
 		if (token[i] <= 47 || token[i] >= 58)
 			return (false);
